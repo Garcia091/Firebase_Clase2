44ce332b4285cd3a7a52fd4b80730147c8d86553
Silvia Garcia <garcia.s091@gmail.com>
2021-05-31 06:33:17 -0500

    Updates

---
+++ precache-manifest.6dbcaf8b5e48f93a1086029d3e3f9b32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b2c4a1aab4f4bd5c603bd02f3330144",
+    "url": "/Firebase_Clase2/index.html"
+  },
+  {
+    "revision": "28e4dbc5020c4b067650",
+    "url": "/Firebase_Clase2/static/css/main.5eafae57.chunk.css"
+  },
+  {
+    "revision": "06c5cc486df3f924b544",
+    "url": "/Firebase_Clase2/static/js/2.c007cc3e.chunk.js"
+  },
+  {
+    "revision": "75fc5a0ff22eb52fcd50272201589016",
+    "url": "/Firebase_Clase2/static/js/2.c007cc3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28e4dbc5020c4b067650",
+    "url": "/Firebase_Clase2/static/js/main.af476a42.chunk.js"
+  },
+  {
+    "revision": "72f806dcf3d657617f5d",
+    "url": "/Firebase_Clase2/static/js/runtime-main.75a6ca47.js"
+  }
+]);
